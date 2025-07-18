ae0f8cc5651ec74258264c48ce635b511ca825d5
Betelhem123 <betelhembiurk8@gmail.com>
2025-07-18 14:45:54 +0300

    env

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,19 @@
 import express from 'express'; 
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
+
+dotenv.config();
 
 const app = express();
+
+// Debug: confirm environment variable is loaded
+console.log('MONGO_URI:', process.env.MONGO_URI);
+
+app.get("/products", (req, res) => {
+  res.send('Products route');
+});
+
 app.listen(5000, () => {
+    connectDB();
     console.log('Server is running on port http://localhost:5000');
-    })
+}); 
